b2ffcc63f498f3221f143bf7bd6f784223e71c64
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2019-11-30 19:56:24 +0100

    arduino project

---
+++ arduino/HX711/HX711.ino
@@ -0,0 +1,23 @@
+#include "HX711.h"
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(115200);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+}
+
+void loop() {
+
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.println(reading);
+  } else {
+    //Serial.println("HX711 not found.");
+    delay(5);
+  }
+}
